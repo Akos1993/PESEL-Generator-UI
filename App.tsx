a79aa00bd722685d25c0a5c0b172bd662849e117
Akos1993 <77202075+Akos1993@users.noreply.github.com>
2026-06-14 20:02:59 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -724,9 +724,6 @@ const App: React.FC = () => {
                       </div>
                     </div>
 
-                    <button type="submit" className="w-full px-4 py-2 bg-blue-600 text-white font-semibold rounded hover:bg-blue-700">
-                      {t('generateIdentity')}
-                    </button>
                   </form>
                 ),
               },
